7f3f0ec2fc8df42a6e386dcced26505b8a8a8e67
RyanSolomonTEST <rsolomon1@ufl.edu>
2021-04-20 13:21:22 -0400

    added checkout function from controller. no error now but not recording the payment in the sandbox

--- client/src/components/profile/Checkout.js
+++ client/src/components/profile/Checkout.js
@@ -1,15 +1,32 @@
 import StripeCheckout from 'react-stripe-checkout';
 
+const stripe = require('stripe')
 const STRIPE_PUBLISHABLE = "pk_test_51IQJj7J37qmk2Gejn9s10LeCsuKOzN5oFpCBRROWmBCg3IkfdJVoKiF4hS0eNSpGLK6lGfbeYd5pqijMU22muHHo00e0VfqHcC";
 
-const onToken = (user,checkout) => token => 
-    checkout(user, token.id);
+    const pay = (req) => {
+        try{    
+                const {source} = req.body;
+                const charge = stripe.charges.create({
+                    amount: '4.99',
+                    currency: 'USD',
+                    source: source
+                })
+                if(charge){
+                    console.log('Payment Success?')
+                }
+                if(!charge) throw Error('Payment failed');
+            }
+        catch(err){
+            console.log(err);
+            }
+        }
 
-const Checkout = ({ amount, user, checkout }) => 
+
+const Checkout = ({ amount, req }) => 
     <StripeCheckout
       amount={amount*100}
-      token={onToken(user,checkout)}
-      currency='INR'
+      token={pay(req)}
+      currency='USD'
       stripeKey={STRIPE_PUBLISHABLE}
 />
 
